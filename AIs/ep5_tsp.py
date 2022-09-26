662548a2b042e112519a827aabd73f75a7471f6c
antoinech <42295406+antoinech2@users.noreply.github.com>
2022-09-26 09:42:30 +0200

    Episode 5

--- AIs/ep5_tsp.py
+++ AIs/ep5_tsp.py
@@ -31,14 +31,16 @@ import time
 def preprocessing (maze_map, maze_width, maze_height, player_location, opponent_location, pieces_of_cheese, time_allowed) :
     global moves
     moves = []
-    start_position = player_location
 
+    meta_graph, paths = build_meta_graph(maze_map, pieces_of_cheese+[player_location])
+    route = tsp(meta_graph, player_location)
+
+    print("route", route)
     #Calculation all paths to get every piece of cheese
-    for cheese_position in pieces_of_cheese:
+    for position_id in range(len(route)-1):
         # We add direction instruction to the queue of movements (FIFO)
         # We calculate path from a piece of cheese to the next one in the list
-        moves += find_route(traversal(start_position, maze_map)[0], start_position, cheese_position)
-        start_position = cheese_position
+        moves += paths[route[position_id]][route[position_id+1]]
     
 
 ##############################################################
@@ -70,35 +72,40 @@ def turn (maze_map, maze_width, maze_height, player_location, opponent_location,
 ##############################################################
 
 def traversal(start_vertex, graph):
-    """Return routing table from the graph traversal starting from a vertex"""
+    """Return routing table from the graph traversal starting from a vertex using Dijkstra’s algorithm"""
 
-    explored_vertices = []
-    queue = []
-    routing_table = {}
-    distances = {}
+    explored_vertices = [] # Contains the list of explored vertices
+    queue = [] # Priority queue of traversal, contains only unexplored vertices
+    routing_table = {} # Dict that will contains the routing table
+    distances = {} # Dict of current distances to all other vertices.
+    # Distance to a vertice not in the dict is infinity
 
-    # We add the initial vertex to the queue
+    # We add the initial vertex to the queue and set its distance to 0
     heapq.heappush(queue, (0, start_vertex))
     distances[start_vertex] = 0
-    routing_table[start_vertex] = None
+    routing_table[start_vertex] = None # No parent for initial vertice
 
     # While the exploring queue is not empty
     while len(queue) > 0:
-        # We pop the first element
+
+        # We pop the unexplored vertice with lowest distance
         cur_weight, cur_vertice = heapq.heappop(queue)
         explored_vertices.append(cur_vertice)
 
-        # We add every unexplored neighbor to the queue
-        #print("nei", list(graph[cur_vertice].items()))
-        start_time = time.time()
         for neighbor in graph[cur_vertice]:
+            # We calculate the new total weight from initial vertice
             total_weight = cur_weight + graph[cur_vertice][neighbor]
+
+            # We add every unexplored neighbor to the queue if not yet explored
             if neighbor not in explored_vertices and (total_weight, neighbor) not in queue:
                 heapq.heappush(queue, (total_weight, neighbor))
+            
+            # We set the new distance if its lower than the previous one
             if neighbor not in distances or total_weight < distances[neighbor]:
-                routing_table[neighbor] = cur_vertice
+                routing_table[neighbor] = cur_vertice # This is the closest parent for now
                 distances[neighbor] = total_weight
 
+    # We return the routing table (representing the shortest path)
     return routing_table, distances
 
 def get_direction_from_neighbors_location(source_location, target_location):
@@ -124,3 +131,58 @@ def find_route (routing_table, source_location, target_location) :
         parent = routing_table[child]
     
     return result_path
+
+
+def build_meta_graph (maze_map, locations) :
+    """Return the meta-graph and all necessary routing tables"""
+    graph = {}
+    paths = {}
+    for cur_loc in locations:
+        routing_table, distances = traversal(cur_loc, maze_map)
+        graph[cur_loc] = {}            
+        paths_from_loc = {}
+        for cur_dest in locations:
+            graph[cur_loc][cur_dest] = distances[cur_dest]
+            paths_from_loc[cur_dest] = find_route(routing_table, cur_loc, cur_dest)
+        paths[cur_loc] = paths_from_loc
+    return graph, paths
+
+def tsp (graph, initial_vertex) :
+    """Solve the TSP using a exhaustive backtracking strategy"""
+    
+    best_length = 9e99
+    best_route = []
+
+    # Recursive implementation of a depth-first search
+    def _tsp (current_vertex, current_length, visited_vertices) :
+        nonlocal best_length
+        nonlocal best_route
+
+        if current_length >= best_length:
+            return
+
+        cur_visited_vertices = visited_vertices.copy()
+
+        # Visiting a vertex
+        #print("Visiting", current_vertex, "at distance", current_length, "from start. Explored :",cur_visited_vertices)
+        cur_visited_vertices.append(current_vertex)
+        
+        # We stop when all vertices are visited
+        if len(cur_visited_vertices) == len(graph) :
+            if current_length < best_length:
+                best_length = current_length
+                best_route = cur_visited_vertices
+                #print(best_length, best_route)
+            return 
+        
+        # If there are still vertices to visit, we explore unexplored neighbors
+        for neighbor in graph[current_vertex] :
+            if neighbor not in cur_visited_vertices :
+                _tsp(neighbor, current_length + graph[current_vertex][neighbor], cur_visited_vertices)
+                
+        # If there are no unvisited neighbors left, the inner function will return,
+        # which corresponds to coming back to the previously visited vertex
+        
+    # Initial call
+    _tsp(initial_vertex, 0, [])
+    return best_route
--- startloc.sh
+++ startloc.sh
@@ -1,6 +1,4 @@
-# python pyrat.py --rat "AIs/improved_random.py"
-#python pyrat.py --rat "AIs/ep3_bfs.py" -md 0.0 -p 1 -x 5 -y 5 --random_seed 1
-#python pyrat.py --rat "AIs/ep3_bfs.py" -md 0.0 -p 1 > "log.txt"
-#python pyrat.py --rat "AIs/ep3_bfs.py" -md 0.0 > "log.txt"
-#python pyrat.py --rat "AIs/ep4_dijkstra.py" -p 1 -x 15 -y 15 -d 0.5 --random_seed 1 > "log.txt"
-python pyrat.py --rat "AIs/ep4_dijkstra.py" -x 25 -y 25 -p 1
+#python pyrat.py --rat "AIs/ep5_tsp.py" -p 1 -x 15 -y 15 -d 0.5 --random_seed 1 > "log.txt"
+
+python pyrat.py --rat "AIs/ep5_tsp.py" -p 11 > log.txt
+#python pyrat.py --rat "AIs/ep5_tsp.py" -x 25 -y 25 --random_seed 1
